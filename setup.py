3957fbef9559ba9f77f92ee972335ecc4cfe403a
Alameddin Çelik <alameddinc@gmail.com>
2024-08-01 15:44:15 +0300

    Generated

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="SilenceCutter",
+    name="silence_cutpy",
     version="1.0",
-    author="Alameddin ÇELİK",
+    author="Alameddin Çelik",
     author_email="alameddinc@gmail.com",
+    description="A tool to remove silent parts from video files",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/alameddinc/silencecut-py",
     packages=find_packages(),
     install_requires=[
         "moviepy",
@@ -12,7 +16,13 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "silence_cutter=silence_cutter:main",
+            "silence_cutter=silence_cutter.main:main",
         ],
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
